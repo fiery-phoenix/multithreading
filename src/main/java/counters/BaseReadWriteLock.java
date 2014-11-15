da1f11687b29c5a291795cc4512fca6131a145fd
Kseniya Panasyuk <kseniya.panasyuk@gmail.com>
2014-11-15 18:53:01 +0200

    Base r/w lock

---
+++ src/main/java/counters/BaseReadWriteLock.java
@@ -0,0 +1,51 @@
+package counters;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Kseniya Panasyuk (KPanasiuk@luxoft.com)
+ */
+public class BaseReadWriteLock {
+
+	private final AtomicInteger writeCount = new AtomicInteger();
+	private final AtomicInteger readCount = new AtomicInteger();
+
+	public void acquireReadLock() {
+		int old = readCount.get();
+		while (old > 0) {
+			if (readCount.compareAndSet(old, old + 1)) {
+				return;
+			}
+			old = readCount.get();
+		}
+
+		while (!writeCount.compareAndSet(0, 1)) {
+		}
+		readCount.incrementAndGet();
+		writeCount.set(0);
+
+	}
+
+	public void releaseReadLock() {
+		while (!decrementCounter(readCount)) {
+		}
+
+	}
+
+	public void acquireWriteLock() {
+		while (!writeCount.compareAndSet(0, 1)) {
+		}
+
+		while (readCount.get() > 0) {
+		}
+	}
+
+	public void releaseWriteLock() {
+		writeCount.set(0);
+	}
+
+	private boolean decrementCounter(AtomicInteger i) {
+		int old = i.get();
+		return i.compareAndSet(old, old == 0 ? 0 : old - 1);
+	}
+}
